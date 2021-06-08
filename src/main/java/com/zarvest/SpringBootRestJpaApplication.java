74b6860b92d3274b3e48e717c7e48e5f1366af36
Gokulakrishnan Mohan <gokul@knowtrichy.com>
2021-06-08 13:00:43 +0530

    first commit

---
+++ src/main/java/com/zarvest/SpringBootRestJpaApplication.java
@@ -0,0 +1,15 @@
+package com.zarvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringBootRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestJpaApplication.class, args);
+	}
+
+}
